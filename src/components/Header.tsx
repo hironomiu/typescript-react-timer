e407de3fae33eb7e46b44fabd5d608e98afbc55b
hironomiu <hironomiu@gmail.com>
2022-06-27 16:11:20 +0900

    commited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="h-14 bg-gray-100 items-center flex">
+    <header className="h-14 bg-gray-100 items-center flex shadow-md">
       <div className="ml-4 text-xl">Timer App</div>
     </header>
   )
