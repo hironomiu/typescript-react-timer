c0fbf6c69bdeeb428d05b02ead1191ede1f1d185
hironomiu <hironomiu@gmail.com>
2022-06-29 14:05:12 +0900

    commited

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <h1 className="text-3xl mb-4">Hello!!</h1>
+      <Link
+        to="/timer"
+        data-testid="timer-link"
+        className="mx-2 text-blue-600 "
+      >
+        Timer
+      </Link>
+      <Link
+        to="/timer2"
+        data-testid="timer2-link"
+        className="mx-2 text-blue-600"
+      >
+        Timer2
+      </Link>
+    </>
+  )
 }
 
 export default Home
