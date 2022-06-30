86d27173a01d6161ada6e335033e1bcfb0b52f47
hironomiu <hironomiu@gmail.com>
2022-06-30 15:42:39 +0900

    commited

--- src/components/parts/Button.tsx
+++ src/components/parts/Button.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { Children } from 'react'
+
+type Color = 'primary' | 'secondary'
 
 interface Props extends React.ComponentPropsWithoutRef<'button'> {
-  color?: 'primary' | 'secondary'
+  color?: Color
   styles?: string
   disabled?: boolean
   onClick?: () => void
   children?: React.ReactNode
 }
+
+const baseStyle = 'h-8 rounded mx-2 bg-blue-400 text-white text-sm font-light'
+
 const Button = ({
   color,
   styles,
@@ -16,13 +21,16 @@ const Button = ({
   children,
   ...props
 }: Props) => {
+  console.log(typeof children === 'string' ? children.length : 0)
   return (
     <button
       type="button"
       className={[
+        baseStyle,
+        `w-[calc(16px*${typeof children === 'string' ? children.length : 5})]`,
         styles,
         `${color ? 'text-blue-300' : ''}`,
-        `${disabled ? 'text-gray-400' : ''}`,
+        `${disabled ? 'disabled:bg-gray-200 disabled:text-gray-400' : ''}`,
       ].join(' ')}
       onClick={onClick}
       disabled={disabled}
--- src/components/timer/TimerButtonBoard.tsx
+++ src/components/timer/TimerButtonBoard.tsx
@@ -14,32 +14,22 @@ const TimerButtonBoard = (props: Props) => {
     <div className="flex mt-2">
       <Button
         onClick={handleClickStart}
-        styles="w-20 h-8 rounded mx-2 bg-blue-400 text-white disabled:bg-gray-200 disabled:text-gray-400 text-sm font-light"
         disabled={
           props.isStart ||
           !(props.hours > 0 || props.minutes > 0 || props.seconds > 0)
         }
-        children="START"
-      />
-      <Button
-        onClick={handleClickRestart}
-        styles="w-24 h-8 rounded mx-2 bg-blue-400 text-white disabled:bg-gray-200 disabled:text-gray-400 text-sm font-light"
-        disabled={isRestart}
-        children="RESTART"
-      />
-      <Button
-        onClick={handleClickStop}
-        styles="w-16 h-8 rounded mx-2 bg-blue-400 text-white disabled:bg-gray-200 disabled:text-gray-400 text-sm font-light"
-        disabled={!props.isStart || !isRestart}
-        children="STOP"
-      />
-
-      <Button
-        onClick={handleClickReset}
-        styles="w-20 h-8 rounded mx-2 bg-blue-400 text-white disabled:bg-gray-200 disabled:text-gray-400 text-sm font-light"
-        disabled={isRestart}
-        children="RESET"
-      />
+      >
+        START
+      </Button>
+      <Button onClick={handleClickRestart} disabled={isRestart}>
+        RESTART
+      </Button>
+      <Button onClick={handleClickStop} disabled={!props.isStart || !isRestart}>
+        STOP
+      </Button>
+      <Button onClick={handleClickReset} disabled={isRestart}>
+        RESET
+      </Button>
     </div>
   )
 }
